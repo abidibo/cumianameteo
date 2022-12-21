041366e44048b42f9d13f35678721a6da0213739
abidibo <abidibo@gmail.com>
2022-12-21 18:10:42 +0100

    Change app logo

--- src/Common/Components/Navbar.js
+++ src/Common/Components/Navbar.js
@@ -2,12 +2,13 @@ import { Box, Heading, useColorMode, } from '@chakra-ui/react'
 import ComponentsTheme from '@Theme/Components'
 import { compose, F, T } from 'ramda'
 import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 import { IoSettingsOutline } from 'react-icons/io5'
+import Logo from './Logo'
 import SettingsModal from './SettingsModal'
 
 const Navbar = () => {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
   const { colorMode } = useColorMode()
   const [settingsModalIsOpen, setSettingsModalIsOpen] = useState(false)
 
@@ -16,7 +17,7 @@ const Navbar = () => {
 
   return (
     <Box bg={ComponentsTheme.navbar.bg[colorMode]} display="flex" height="100%" alignItems="center" justifyContent="space-between" px={5}>
-      <Heading colorScheme="orange" color='orange' fontSize={'1.6rem'}>{t('ui.AppTitle')}</Heading>
+      <Heading colorScheme="orange" color='orange' fontSize={'1.6rem'}><Logo width='200px' height='34px' textColor={'orange'} iconColor='#44B2AE' /></Heading>
       <Box display="flex" gap={1}>
         <IoSettingsOutline size={24} onClick={handleOpenSettings} style={{ cursor: 'pointer' }} />
       </Box>
